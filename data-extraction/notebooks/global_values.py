d1f871338f1245a41af305dece7e9dc6bdf6b7c2
Ângelo Antônio Bertoli Guido <45051273+angelobguido@users.noreply.github.com>
2024-10-18 22:28:33 -0300

    Add notebook files to transform the raw csv to processed parquet file

---
+++ data-extraction/notebooks/global_values.py
@@ -0,0 +1,82 @@
+state_codes = {
+    0: "Não informado",
+    11: "Rondônia",
+    12: "Acre",
+    13: "Amazonas",
+    14: "Roraima",
+    15: "Pará",
+    16: "Amapá",
+    17: "Tocantins",
+    21: "Maranhão",
+    22: "Piauí",
+    23: "Ceará",
+    24: "Rio Grande do Norte",
+    25: "Paraíba",
+    26: "Pernambuco",
+    20: "Pernambuco",
+    27: "Alagoas",
+    28: "Sergipe",
+    29: "Bahia",
+    31: "Minas Gerais",
+    32: "Espírito Santo",
+    33: "Rio de Janeiro",
+    35: "São Paulo",
+    41: "Paraná",
+    42: "Santa Catarina",
+    43: "Rio Grande do Sul",
+    50: "Mato Grosso do Sul",
+    51: "Mato Grosso",
+    52: "Goiás",
+    53: "Distrito Federal"
+}
+
+uf_codes = {
+    "RO": 11,
+    "AC": 12,
+    "AM": 13,
+    "RR": 14,
+    "PA": 15,
+    "AP": 16,
+    "TO": 17,
+    "MA": 21,
+    "PI": 22,
+    "CE": 23,
+    "RN": 24,
+    "PB": 25,
+    "PE": 26,
+    "AL": 27,
+    "SE": 28,
+    "BA": 29,
+    "MG": 31,
+    "ES": 32,
+    "RJ": 33,
+    "SP": 35,
+    "PR": 41,
+    "SC": 42,
+    "RS": 43,
+    "MS": 50,
+    "MT": 51,
+    "GO": 52,
+    "DF": 53
+}
+
+skin_color_codes = {
+    0: "Ign/Branco",
+    1: "Branca",
+    2: "Preta",
+    3: "Amarela",
+    4: "Parda",
+    5: "Indigena"
+}
+
+sex_codes = {
+    0: "Ignorado",
+    1: "Masculino",
+    2: "Feminino",
+    3: "Feminino"
+}
+
+death_code = {
+    0: "Com óbito",
+    1: "Sem óbito"
+}
