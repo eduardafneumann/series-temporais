f02242d43263faab73016b4bd295e0a04778e940
Ângelo Antônio Bertoli Guido <45051273+angelobguido@users.noreply.github.com>
2024-10-22 09:25:05 -0300

    Add multi graph functionality with filters

--- frontend/pages/bases/sinan.py
+++ frontend/pages/bases/sinan.py
@@ -2,24 +2,92 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-col = st.columns(2, gap="small", vertical_alignment="top")
+# Load data
+@st.cache_data
+def load_data():
+    df_zika = pd.read_parquet('data-extraction/parquet_data/ZIKA.parquet')
+    df_chik = pd.read_parquet('data-extraction/parquet_data/CHIK.parquet')
+    df_deng = pd.read_parquet('data-extraction/parquet_data/DENG.parquet')
+    df_aids = pd.read_parquet('data-extraction/parquet_data/AIDS.parquet')
+    return df_zika, df_chik, df_deng, df_aids
 
-df_zika = pd.read_csv('data/sinan/zika_serie_anual.csv')
-df_aids = pd.read_csv('data/sinan/aids_serie_anual.csv')
-df_chik = pd.read_csv('data/sinan/chik_serie_anual.csv')
-df_deng = pd.read_csv('data/sinan/dengue_serie_anual.csv')
+df_zika, df_chik, df_deng, df_aids = load_data()
 
-with st.container():
-    with col[0]:
-        st.plotly_chart(px.line(df_zika, x='ANO', y='CASOS', title='Casos de zika por ano'), key=1)
+# Add a multiselect box for selecting dataframes
+dfs_option = st.multiselect(
+    'Select Dataframes',
+    options=['ZIKA', 'CHIK', 'DENG', 'AIDS'],
+    default=['ZIKA', 'CHIK', 'DENG', 'AIDS']
+)
 
-    with col[1]:
-        st.plotly_chart(px.line(df_aids, x='ANO', y='CASOS', title='Casos de AIDS por ano'), key=2)
+# Add a slider for selecting the time period
+start_date, end_date = st.slider(
+    'Select Time Period',
+    min_value=pd.to_datetime('2000-01-01').date(),
+    max_value=pd.to_datetime('2023-12-31').date(),
+    value=(pd.to_datetime('2020-01-01').date(), pd.to_datetime('2023-12-31').date())
+)
 
-with st.container():
-    with col[0]:
-        st.plotly_chart(px.line(df_chik, x='ANO', y='CASOS', title='Casos de chikungunya por ano'), key=4)
+# Add a selectbox for filtering by "sexo"
+sexo_option = st.selectbox(
+    'Select Gender',
+    options=[-1, 0, 1, 2],
+    format_func=lambda x: 'All' if x == -1 else 'Not Identified' if x == 0 else 'Male' if x == 1 else 'Female'
+)
 
-    with col[1]:
-        st.plotly_chart(px.line(df_deng, x='ANO', y='CASOS', title='Casos de dengue por ano'), key=3)
+# Add a selectbox for filtering by "raca"
+raca_option = st.selectbox(
+    'Select Race',
+    options=[-1, 0, 1, 2, 3, 4, 5],
+    format_func=lambda x: 'All' if x == -1 else 'Not Identified' if x == 0 else 'White' if x == 1 else 'Black' if x == 2 else 'Yellow' if x == 3 else 'Brown' if x == 4 else 'Indigenous'
+)
 
+# Add a slider for filtering by "idade"
+idade_option = st.slider(
+    'Select Age',
+    min_value=0,
+    max_value=150,
+    value=(0, 150)
+)
+
+# Function to filter dataframe by time
+@st.cache_data
+def filter_df_time(df, start_date, end_date):
+    return df[(df.index.date >= start_date) & (df.index.date <= end_date)]
+
+
+# Function to filter dataframe
+def filter_df(df, start_date, end_date, sexo_option, raca_option, idade_option, disease):
+    df = filter_df_time(df, start_date, end_date)
+    if sexo_option != -1:
+        df = df[df['sexo'] == sexo_option]
+    if raca_option != -1:
+        df = df[df['raca'] == raca_option]
+    df = df[(df['idade'] >= idade_option[0]) & (df['idade'] <= idade_option[1])]
+
+
+    df = df.resample('D').count()
+    df = df[['sexo']]
+    df = df.rename(columns={'sexo': 'casos'})
+    df['doenca'] = disease
+
+    return df
+
+# Filter selected dataframes
+filtered_dfs = []
+if 'ZIKA' in dfs_option:
+    filtered_dfs.append(filter_df(df_zika, start_date, end_date, sexo_option, raca_option, idade_option, 'ZIKA'))
+if 'CHIK' in dfs_option:
+    filtered_dfs.append(filter_df(df_chik, start_date, end_date, sexo_option, raca_option, idade_option, 'CHIK'))
+if 'DENG' in dfs_option:
+    filtered_dfs.append(filter_df(df_deng, start_date, end_date, sexo_option, raca_option, idade_option, 'DENG'))
+if 'AIDS' in dfs_option:
+    filtered_dfs.append(filter_df(df_aids, start_date, end_date, sexo_option, raca_option, idade_option, 'AIDS'))
+
+# Combine filtered dataframes
+if filtered_dfs:
+    combined_df = pd.concat(filtered_dfs)
+    
+    # Plot line chart for the combined data
+    fig = px.line(combined_df, x=combined_df.index, y='casos', color='doenca', title='Casos por Doença')
+    st.plotly_chart(fig, key=1)
--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -2,9 +2,9 @@ import streamlit as st
 
 pages = {
     "Bases": [
-        st.Page("pages/bases/sih.py", title="SIH"),
-        st.Page("pages/bases/sim.py", title="SIM"),
-        st.Page("pages/bases/sinan.py", title="SINAN"),
+        #st.Page("pages/bases/sih.py", title="SIH"),
+        #st.Page("pages/bases/sim.py", title="SIM"),
+        st.Page("pages/bases/sinan.py", title="SINAN")
     ],
     "Insights": [
         st.Page("pages/insights.py", title="Insight 1"),
