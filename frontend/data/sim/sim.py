c13cd47ec698874c773fc650553d33d706479571
Ângelo Antônio Bertoli Guido <45051273+angelobguido@users.noreply.github.com>
2024-10-26 15:19:46 -0300

    Remove some files, and deng processing
    I did that trying to reduce the amount of memory required to run.

--- frontend/data/sim/sim.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv('sim.csv')
-df['ANO'] = df.apply(lambda row: str(row.DTOBITO)[-4:], axis = 1)
-new_df = pd.DataFrame(data=df.value_counts(subset=['ANO']))
-new_df.columns = ['OBITOS']
-new_df.sort_index(inplace=True)
-new_df.to_csv('sim_serie_anual.csv')
